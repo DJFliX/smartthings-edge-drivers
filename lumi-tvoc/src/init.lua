0f23189d0886fdbb99da4b4e6280af6a6aaff470
Felix Mann <djflix@gmail.com>
2021-11-28 10:13:14 +0100

    Fix TVOC reporting by removing faulty refresh code + incorporate code from varzac

--- lumi-tvoc/src/init.lua
+++ lumi-tvoc/src/init.lua
@@ -2,78 +2,84 @@ local capabilities = require "st.capabilities"
 local ZigbeeDriver = require "st.zigbee"
 local defaults = require "st.zigbee.defaults"
 local utils = require "st.utils"
-local log = require "log"
-local zcl_clusters = require "st.zigbee.zcl.clusters" 
+local zcl_clusters = require "st.zigbee.zcl.clusters"
 local data_types = require "st.zigbee.data_types"
 local cluster_base = require "st.zigbee.cluster_base"
+local device_management = require "st.zigbee.device_management"
+local battery_defaults = require "st.zigbee.defaults.battery_defaults"
 
-local temperature_value_attr_header = function(driver, device, value, zb_rx)
-  local temperature = value.value / 100
+-- A lot of the following was heavily inspired by the following sources:
+-- * https://github.com/veonua/SmartThingsEdge-Xiaomi < The base structure of the file (which of course changed over time)
+-- * https://github.com/varzac/EdgeDrivers/blob/aqara-tvoc/xiaomi-sensor/src/aqara_air_sensor.lua < The fact that it is possible to register for default handlers for temperature + humidity :mindblown:
 
-  if temperature < -99 or temperature > 99 then
-    log.info("Temperature value out of range: " .. temperature)
-    return
+local ANALOG_INPUT_CLUSTER = 0x000C
+local ANALOG_INPUT_PRESENT_VALUE = 0x0055
+
+local function round_to(val, unit_val)
+  local mult = 1 / unit_val
+  if mult % 1 ~= 0 then
+    error("unit_val should be a power of 10")
   end
-
-  device:emit_event(capabilities.temperatureMeasurement.temperature({ value = temperature, unit = "C" }))
-end
-
-local humidity_value_attr_handler = function(driver, device, value, zb_rx)
-  local percent = utils.clamp_value(value.value / 100, 0.0, 100.0)
-  if percent<99 then -- filter out spurious values
-    device:emit_event(capabilities.relativeHumidityMeasurement.humidity(percent))
-  end
+  return (utils.round(val * mult)) * unit_val
 end
 
 local tvoc_value_attr_handler = function(driver, device, value, zb_rx)
-  local tvoc_ppb = utils.clamp_value(value.value, 0.0, 2500.0)
-  device:emit_event(capabilities.tvocMeasurement.tvocLevel({value = tvoc_ppb, unit = "ppm"}))
+  local tvoc_ppm = value.value / 1000 -- Value is in ppb
+  device:emit_event(capabilities.tvocMeasurement.tvocLevel(round_to(tvoc_ppm, .001)))
 end
 
-local battery_voltage_attr_handler = function(driver, device, value, zb_rx)
-  local raw_bat_volt = value.value / 10
-  local raw_bat_perc = (raw_bat_volt - 2.5) * 100 / (3.0 - 2.5)
-  local bat_perc = math.floor(math.max(math.min(raw_bat_perc, 100), 0))
-  device:emit_event(capabilities.battery.battery(bat_perc))
+local do_configure = function(self, device)
+  device:configure()
+  device:refresh()
+
+  device:send(device_management.attr_refresh(device, ANALOG_INPUT_CLUSTER, ANALOG_INPUT_PRESENT_VALUE))
+  device:send(device_management.build_bind_request(device, ANALOG_INPUT_CLUSTER, self.environment_info.hub_zigbee_eui))
+
+  local analog_cluster_present_value_conf = {
+    cluster = ANALOG_INPUT_CLUSTER,
+    attribute = ANALOG_INPUT_PRESENT_VALUE,
+    minimum_interval = 60, -- 30 seconds
+    maximum_interval = 900, -- 10 minutes
+    data_type = data_types.SinglePrecisionFloat,
+    -- 10.0 ppb or .01 ppm
+    reportable_change = data_types.SinglePrecisionFloat(0, 3, .25)
+  }
+
+  device:send(device_management.attr_config(device, analog_cluster_present_value_conf))
+
 end
 
-local function refresh_handler(driver, device, command)
-  device:send(zcl_clusters.TemperatureMeasurement.attributes.MeasuredValue:read(device))
-  device:send(zcl_clusters.RelativeHumidity.attributes.MeasuredValue:read(device))
-  device:send(cluster_base.read_attribute(device, data_types.ClusterId(0x000C), data_types.AttributeId(0x0055)))
+local function identify_handler(driver, device, zb_rx)
+  device:send(device_management.attr_refresh(device, ANALOG_INPUT_CLUSTER, ANALOG_INPUT_PRESENT_VALUE))
 end
+
 
 local zigbee_temp_driver_template = {
-  supported_capabilities = {
-    capabilities.relativeHumidityMeasurement,
-    capabilities.temperatureMeasurement,
-    capabilities.tvocMeasurement,
-    capabilities.battery
-  },
+  supported_capabilities = {capabilities.relativeHumidityMeasurement, capabilities.temperatureMeasurement,
+                            capabilities.tvocMeasurement, capabilities.battery,
+                            capabilities.firmwareUpdate},
   use_defaults = true,
-  capability_handlers = {
-    [capabilities.refresh.ID] = {
-      [capabilities.refresh.commands.refresh.NAME] = refresh_handler
-    }
+  lifecycle_handlers = {
+    init = battery_defaults.build_linear_voltage_init(2, 3),
+    doConfigure = do_configure
   },
   zigbee_handlers = {
     attr = {
-      [zcl_clusters.PowerConfiguration.ID] = {
-        [zcl_clusters.PowerConfiguration.attributes.BatteryVoltage.ID] = battery_voltage_attr_handler
-      },
-      [zcl_clusters.TemperatureMeasurement.ID] = {
-        [zcl_clusters.TemperatureMeasurement.attributes.MeasuredValue.ID] = temperature_value_attr_header
-      },
-      [zcl_clusters.RelativeHumidity.ID] = {
-        [zcl_clusters.RelativeHumidity.attributes.MeasuredValue.ID] = humidity_value_attr_handler
-      },
-      [0x000C] = { -- AnalogInput
-        [0x0055] = tvoc_value_attr_handler -- PresentValue (single)
+      [ANALOG_INPUT_CLUSTER] = { -- AnalogInput
+        [ANALOG_INPUT_PRESENT_VALUE] = tvoc_value_attr_handler -- PresentValue (single)
+      }
+    },
+    cluster = {
+      [zcl_clusters.Identify.ID] = {
+        [zcl_clusters.Identify.commands.IdentifyQuery.ID] = identify_handler
       }
     }
-  },
+  }
 }
 
-defaults.register_for_default_handlers(zigbee_temp_driver_template, zigbee_temp_driver_template.supported_capabilities)
+defaults.register_for_default_handlers(zigbee_temp_driver_template,
+  {capabilities.temperatureMeasurement, capabilities.relativeHumidityMeasurement, capabilities.battery,
+   capabilities.firmwareUpdate})
+
 local driver = ZigbeeDriver("aqara_tvoc", zigbee_temp_driver_template)
 driver:run()
